db09fbe8ddad8ce9e652d0b65c5766acefe68f49
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-02 12:05:21 +0100

    View models defined

---
+++ SoundJabber/SoundJabber/ViewModels/SoundGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundJabber.ViewModels
+{
+    public class SoundGroup
+    {
+        public SoundGroup()
+        {
+            Items = new List<SoundData>();
+        }
+        public string Title { get; set; }
+
+        public List<SoundData> Items { get; set; }
+    }
+}
---
+++ SoundJabber/SoundJabber/ViewModels/SoundModel.cs
@@ -0,0 +1,54 @@
+﻿
+namespace SoundJabber.ViewModels
+{
+    public class SoundModel
+    {
+        public SoundGroup Animals { get; set; }
+        public SoundGroup Cartoons { get; set; }
+        public SoundGroup Warnings { get; set; }
+        public SoundGroup CustomSounds { get; set; }
+        public SoundGroup Taunts { get; set; }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        public void LoadData()
+        {
+            Animals = CreateAnimalsGroup();
+            Cartoons = CreateCartoonsGroup();
+            Taunts = CreateTauntsGroup();
+            Warnings = CreateWarningsGroup();
+            CustomSounds = CreateCustomSoundsGroup();
+
+            IsDataLoaded = true;
+        }
+
+        private SoundGroup CreateCustomSoundsGroup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private SoundGroup CreateWarningsGroup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private SoundGroup CreateTauntsGroup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private SoundGroup CreateCartoonsGroup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private SoundGroup CreateAnimalsGroup()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
