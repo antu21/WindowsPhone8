6a9fb1bf9238c2b314cfe2c97b4908b6332ec626
Prasad Honrao <Honrao.Prasad@Gmail.com>
2013-08-28 16:27:29 +0100

    Removed unused namespaces

--- SoundJabber/SoundJabber/Resources/AppResources.Designer.cs
+++ SoundJabber/SoundJabber/Resources/AppResources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SoundJabber.Resources {
-    using System;
-    
-    
+namespace SoundJabber.Resources
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
